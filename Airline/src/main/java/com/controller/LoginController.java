dbf11c2fe8244c9d71a55b9c8092836c0ee8a3aa
Shivansh Keshari <eshukeshari@gmail.com>
2021-03-02 17:06:42 +0530

    Added Cross Origin

--- Airline/src/main/java/com/controller/LoginController.java
+++ Airline/src/main/java/com/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.bean.Login;
 import com.service.LoginService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "login")
 public class LoginController {
 
