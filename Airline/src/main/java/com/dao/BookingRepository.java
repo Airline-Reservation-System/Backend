efe5a277f38f300752e3bff2c135f630a9c88d5d
Abhishek Srivastava <abhisheksrivastava0990@gmail.com>
2021-03-16 19:27:58 +0530

    final project

--- Airline/src/main/java/com/dao/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Booking;
-import javax.persistence.EntityManagerFactory;
-public interface BookingRepository extends JpaRepository<Booking,Integer>{
-
-}
--- Airline/src/main/java/com/dao/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Flight;
-
-public interface FlightRepository extends JpaRepository<Flight, Integer>  {
-
-}
